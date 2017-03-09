ef3f3b6b3562300b3b6948329e2b0be312853256
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-03-09 06:52:33 +0000

    scraping Birthday Flowers info

--- lib/Florist/bday.rb
+++ lib/Florist/bday.rb
@@ -4,43 +4,45 @@ require 'date'
 
 
 class Birthday
-attr_accessor :flower_name,:price, :url
-@@all = []
+attr_accessor :flower_name, :url
+
 BASE_PATH = "./fixtures/florist_site/birthday.html"
 def initialize()
-  #@url = BASE_PATH
-  #binding.pry
-  scraping_flower_name
+    display_deals(scraping_bouquets_info)
 end
 
-def scraping_flower_name
-  deals = []
-  doc = Nokogiri::HTML(open(BASE_PATH))
-  welcome_text = doc.css('title').text.to_s
-  #outofstock = doc.css('#cond_msg').text
-
-  puts "you are in #{welcome_text} \n Would you like to see best deals as of #{Time.now.strftime("%d/%m/%Y %H:%M")} Plese Type Y/N
-  \n******************************************************************************"
-  input = gets.chomp
-  if input.upcase == "Y"
-    todays_deals
-  else
-    puts "How can we help you?"
-  end
+def scraping_bouquets_info
+      doc = Nokogiri::HTML(open(BASE_PATH))
+      welcome_text = doc.css('title').text.to_s
+      #outofstock = doc.css('#cond_msg').text
+      puts "you are in #{welcome_text} \n Would you like to see best deals as of #{Time.now.strftime("%d/%m/%Y %H:%M")} Plese Type Y/N
+      "
+      input = gets.chomp
+      if input.upcase == "Y"
+        todays_deals
+      else
+        puts "How can we help you?"
+      end
 end
 
 def todays_deals
-#scrape from url
-deals = []
-doc = Nokogiri::HTML(open(BASE_PATH))
-doc.css(".product_row_cont").each{|e| puts e.text.gsub(/\n+/, " \n")
-puts "\n******************************************************************************"}
-
+    doc = Nokogiri::HTML(open(BASE_PATH))
+    all_deals = []
+    doc.css(".product_row_cont").each{|bouquet|
+    deals = {}
+    deals[:flower] =  bouquet.css("img").attribute("title").value # scraping bouquet description
+    deals[:price] =  bouquet.css(".price_span").first.text        # scraping price for the bouquet
+    all_deals << deals
+  }
+    all_deals
 end
 
-def self.all
-  @all
+def display_deals(deals)
+    deals.each{|deal|
+    puts "Bouquet Description :-" + deal[:flower]
+    puts "Price :- " + deal[:price]
+    puts "-------------------------------------------------------------"
+    }
 end
-
 
 end
